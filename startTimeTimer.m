a0851d8fe09c8b88421a3c64cb4db2b273cd4a98
Michal Stanke <michal@stanke.cz>
2017-01-17 18:43:32 +0100

    Změna rychlosti času bez nutnosti restartu timeru (#9)

--- startTimeTimer.m
+++ startTimeTimer.m
@@ -7,21 +7,6 @@ end
 function timeTimerFcn( timeTimer, ~ )
     userData = timeTimer.UserData;
     % increment elapsed days
-    userData.controlsContainer.UserData.currentDate = userData.controlsContainer.UserData.currentDate + days(1);
-    
-    % set new period based on the speed control
-    newPeriod = 0.1/userData.controlsContainer.UserData.speedControl.Value;
-    setTimerPeriod( timeTimer, newPeriod );
-end
-
-function setTimerPeriod( timer, newPeriod )
-    % stop the timer to allow update
-    stop(timer);
-    % strip the new period to milliseconds
-    newPeriod = newPeriod - (mod(newPeriod, 0.001));
-    % set new timer start delay and period
-    timer.StartDelay = newPeriod;
-    timer.Period = newPeriod;
-    % start the timer again
-    start(timer);
+    speedValue = userData.controlsContainer.UserData.speedControl.Value;
+    userData.controlsContainer.UserData.currentDate = userData.controlsContainer.UserData.currentDate + days(speedValue);
 end
