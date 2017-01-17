d84f727997258a6d0aac7674b96aa251c662adbf
Michal Stanke <michal@stanke.cz>
2017-01-17 18:44:54 +0100

    Nastavení rozsahu os plotu (#5)

--- addVisualizationContainer.m
+++ addVisualizationContainer.m
@@ -10,6 +10,8 @@ end
 function initVisualization( container )
     % create visualization subplot
     ax = subplot(1, 1, 1, 'Parent', container);
+    ax.XLim = [-40 40];
+    ax.YLim = [-40 40];
     grid(ax, 'on');
     view(ax, 3);
 end
